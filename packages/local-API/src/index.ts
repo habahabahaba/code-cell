43908244d45bce90371ba83b63f7e5ca32e36f90
Alex S <habahabahaba@outlook.com>
2024-07-14 01:50:37 +0400

    First implementation of serving local-client with local-API.

--- packages/local-API/src/index.ts
+++ packages/local-API/src/index.ts
@@ -1,7 +1,10 @@
 // 3rd party:
-import { createProxyMiddleware } from 'http-proxy-middleware';
+// import { createProxyMiddleware } from 'http-proxy-middleware';
 // Express:
 import express from 'express';
+// Node:
+import { dirname } from 'path';
+import { createRequire } from 'module';
 
 export function serve(
   port: number,
@@ -10,13 +13,22 @@ export function serve(
 ): Promise<void> {
   const app = express();
 
-  app.use(
-    createProxyMiddleware({
-      target: `http://localhost:5173`,
-      ws: true,
-      //   logger: null, // logLevel: 'silent' is no longer available;
-    })
-  );
+  // Resolve the client path
+  //   console.log('import.meta.url: ', import.meta.url);
+  const require = createRequire(import.meta.url);
+  const clientPath = require.resolve('@code-cell/local-client/dist/index.html');
+  //   console.log('clientPath: ', clientPath);
+
+  // Serve static files from the directory containing the resolved client path
+  app.use(express.static(dirname(clientPath)));
+
+  //   app.use(
+  //     createProxyMiddleware({
+  //       target: `http://localhost:5173`,
+  //       ws: true,
+  //       //   logger: null, // logLevel: 'silent' is no longer available;
+  //     })
+  //   );
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
