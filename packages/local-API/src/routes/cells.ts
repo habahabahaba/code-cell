650db07b4596b5923a7ef299358f0da0332f15b7
Alex S <habahabahaba@outlook.com>
2024-07-15 02:30:12 +0400

    Adding data fetching to local-API. WIP.

--- packages/local-API/src/routes/cells.ts
+++ packages/local-API/src/routes/cells.ts
@@ -3,9 +3,15 @@
 import express from 'express';
 // Node:
 import fs from 'fs/promises';
+import path from 'path';
 
 // Types, interfaces and enumns:
 import type { Router } from 'express';
+interface Cell {
+  id: string;
+  content: string;
+  type: 'code' | 'text';
+}
 
 export default function createCellsRouter(
   filename: string,
@@ -13,17 +19,44 @@ export default function createCellsRouter(
 ): Router {
   const router = express.Router();
 
+  router.use(express.json());
+
+  const filePath = path.join(dir, filename);
+
   router.get('/cells', async (request, response) => {
-    // Checking if the cells storage file exists:
-    // If no such file exists, add a default list of cells:
-    // Reading the file:
-    // Parsing a list of cells:
-    // Sending list of cells back to the browser:
+    try {
+      // Trying to read the file:
+      const result = await fs.readFile(filePath, { encoding: 'utf-8' });
+
+      // Parsing a list of cells:
+      const parsedResult = JSON.parse(result);
+      // Sending list of cells back to the browser:
+
+      response.send(parsedResult);
+    } catch (error) {
+      if (
+        typeof error === 'object' &&
+        error !== null &&
+        'code' in error &&
+        error.code === 'ENOENT'
+      ) {
+        // If no such file exists, add a default list of cells:
+        await fs.writeFile(filePath, '[]', 'utf-8');
+        response.send([]);
+      } else {
+        throw error;
+      }
+    }
   });
   router.post('/cells', async (request, response) => {
     // Acquiring a list of cells from the request object:
+    const { cells }: { cells: Cell[] } = request.body;
     // Serializing the list of cells:
+    const serializedCells = JSON.stringify(cells);
     // Writing the cells to the file:
+    await fs.writeFile(filePath, serializedCells, 'utf-8');
+
+    response.status(201).send('The file was successfully created');
   });
 
   return router;
